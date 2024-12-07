11952594e0f4ad1145253df1649a49ce7d5351ff
Sanzhar <sariev.sanzhar.t@gmail.com>
2024-12-07 20:48:42 +0500

    conected with back

--- src/Routes/List.jsx
+++ src/Routes/List.jsx
@@ -1,40 +1,41 @@
+import { useEffect, useState } from "react";
 import Item from "../components/Item";
 
 export default function List() {
-  const list = [
-    {
-      name: "1",
-      title: "esgafharh",
-      date: "01/01424051/34634/602",
-      link: "phub.com",
-      descr: "huynya ebanaya",
-      id: "007",
-    },
-    {
-      name: "2",
-      title: "sdgarhaerh",
-      date: "01/01424051/34634/602",
-      link: "phub.com",
-      descr: "huynya ebanaya",
-      id: "007",
-    },
-    {
-      name: "3",
-      title: "qwegqweg",
-      date: "01/01424051/34634/602",
-      link: "phub.com",
-      descr: "huynya ebanaya",
-      id: "007",
-    },
-  ];
+  const [vulnerabilities, setVulnerabilities] = useState([]);
+
+  const getData = async () => {
+    const response = await fetch(
+      "http://127.0.0.1:5000/api/v1/vulnerabilities"
+    ); // Make sure to use the full URL with http://
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    setVulnerabilities(data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div>
-      {list.map((el) => (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {vulnerabilities?.map((el) => (
         <Item
+          key={el.id}
           date={el.date}
           link={el.link}
           title={el.title}
-          description={el.descr}
+          description={el.description}
           id={el.id}
         />
       ))}
